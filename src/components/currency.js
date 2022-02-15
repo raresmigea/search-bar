48f8867a5365e6738a12bb84504c9f0de5a073a0
raresmigea <raresini@yahoo.com>
2022-02-15 22:57:02 +0200

    removed code

--- src/components/currency.js
+++ src/components/currency.js
@@ -5,9 +5,13 @@ import ListItem from '@material-ui/core/ListItem';
 const Currency = (data) => {
   //   console.log('Currency data: ', data);
   return (
-    <ListItem>
-      <div>{data.data.currency}</div>
-      <div>{data.data.exchangeRate?.middle}</div>
+    <ListItem style={{ height: 100 }}>
+      <div>
+        <div>Currency: {data.data.currency}</div>
+        <div>Name: {data.data.nameI18N}</div>
+        <div>Buy: {data.data.exchangeRate?.buy}</div>
+        <div>Sell: {data.data.exchangeRate?.sell}</div>
+      </div>
     </ListItem>
   );
 };
