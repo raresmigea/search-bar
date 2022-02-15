dc1bd936258af202ac3344eabcdc6f0c30db584c
raresmigea <raresini@yahoo.com>
2022-02-15 22:52:54 +0200

    removed code

--- src/App.js
+++ src/App.js
@@ -2,14 +2,9 @@ import React from 'react';
 import { QueryClient, QueryClientProvider } from 'react-query';
 import './App.css';
 import Fetch from './components/fetch';
-// import useGetData from './components/use-get-data';
 const queryClient = new QueryClient({});
 
 function App() {
-  // const { data, isLoading } = useGetData();
-  // console.log('data: ', data);
-  // console.log('isLoading: ', isLoading);
-  // the value of the search field
   return (
     <QueryClientProvider client={queryClient}>
       <div className='container'>
