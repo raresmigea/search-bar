5312105d66a5995c1bcfb9c82939b2b7075bea21
raresmigea <raresini@yahoo.com>
2022-02-15 23:04:16 +0200

    test flag

--- src/components/currency.js
+++ src/components/currency.js
@@ -1,9 +1,13 @@
 import React from 'react';
 
 import ListItem from '@material-ui/core/ListItem';
-
+import img from '../flags/ad.png';
 const Currency = (data) => {
   //   console.log('Currency data: ', data);
+  const flag = `../flags/${data.data.currency
+    .substring(0, 2)
+    .toLowerCase()}.png`;
+  console.log('fff ', flag);
   return (
     <ListItem style={{ height: 100 }}>
       <div>
@@ -11,6 +15,8 @@ const Currency = (data) => {
         <div>Name: {data.data.nameI18N}</div>
         <div>Buy: {data.data.exchangeRate?.buy}</div>
         <div>Sell: {data.data.exchangeRate?.sell}</div>
+        {/* <img src={flag} alt='this is a flag' /> */}
+        <img src={img} alt='this is a flag' />
       </div>
     </ListItem>
   );
