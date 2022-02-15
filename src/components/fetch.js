58cd499045635c847828efb03abddd15a97b0e39
raresmigea <raresini@yahoo.com>
2022-02-15 22:32:38 +0200

    in progess

--- src/components/fetch.js
+++ src/components/fetch.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { useQuery } from 'react-query';
 import Currency from './currency';
@@ -7,9 +7,9 @@ import Paper from '@material-ui/core/Paper';
 
 const Fetch = () => {
   const { isLoading, isError, data, error, refetch, isSuccess } = useQuery(
-    'joke',
+    'fetch',
     async () => {
-      const { data } = await axios(
+      const data = await axios(
         'https://run.mocky.io/v3/c88db14a-3128-4fbd-af74-1371c5bb0343'
       );
       return data;
@@ -24,12 +24,24 @@ const Fetch = () => {
   //   return <h1>{error}</h1>;
   // }
 
+  console.log('data: ', data);
+
+  const [list, setList] = useState([]);
+
+  useEffect(() => {
+    if (isSuccess && !isLoading) {
+      setList(data.data.fx);
+    }
+  }, [isSuccess, isLoading, data.data.fx]);
+
+  console.log('list ', list);
+
   return (
     <>
       {isSuccess && (
         <Paper style={{ maxHeight: 400, overflow: 'auto' }}>
           <List>
-            {data.fx.map((a, index) => (
+            {list.map((a, index) => (
               <Currency data={a} key={index} />
             ))}
           </List>
