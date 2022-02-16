2765c894ec43f7cab67e959e9066084f9e751940
raresmigea <raresini@yahoo.com>
2022-02-16 22:22:45 +0200

    refactoring

--- src/components/container/container.js
+++ src/components/container/container.js
@@ -9,23 +9,17 @@ import Box from '@material-ui/core/Box';
 import './container.css';
 
 const Container = () => {
-  const { isLoading, data, isSuccess } = useQuery('fetch', async () => {
-    const data = await axios(
-      'https://run.mocky.io/v3/c88db14a-3128-4fbd-af74-1371c5bb0343'
-    );
-    return data;
-  });
-
   const [list, setList] = useState([]);
   const [searchedList, searchedSetList] = useState([]);
   const [name, setName] = useState('');
-
-  useEffect(() => {
-    if (isSuccess && !isLoading) {
-      setList(data.data.fx);
-      searchedSetList(data.data.fx);
-    }
-  }, [isSuccess, isLoading, data?.data?.fx]);
+  const query = 'getCurrencies';
+  const { isLoading, data, isSuccess, isError } = useQuery(query, async () => {
+    const data = await axios(
+      'https://run.mocky.io/v3/c88db14a-3128-4fbd-af74-1371c5bb0343'
+    );
+    const response = data.data.fx;
+    return response;
+  });
 
   const filter = (e) => {
     const keyword = e.target.value;
@@ -35,36 +29,50 @@ const Container = () => {
       });
       setList(results);
     } else {
-      setList(data.data.fx);
+      setList(data);
     }
-
     setName(keyword);
   };
 
+  useEffect(() => {
+    if (isSuccess && !isLoading) {
+      setList(data);
+      searchedSetList(data);
+    }
+  }, [isSuccess, isLoading, data]);
+
   return (
-    <Box variant='outlined'>
-      <TextField
-        label='Search by currency code'
-        type='search'
-        value={name}
-        onChange={filter}
-        className='input'
-        variant='outlined'
-      />
-      {isSuccess ? (
-        <Paper className='paper' variant='outlined'>
-          <List className='list'>
-            {list.length ? (
-              list.map((element, index) => (
-                <Currency data={element} key={`currency-${index}`} />
-              ))
-            ) : (
-              <div className='no-results'>no found results</div>
-            )}
-          </List>
-        </Paper>
-      ) : null}
-    </Box>
+    <>
+      {!isLoading ? (
+        <Box variant='outlined'>
+          <TextField
+            label='Search by currency code'
+            type='search'
+            value={name}
+            onChange={filter}
+            className='input'
+            variant='outlined'
+          />
+          {isSuccess ? (
+            <Paper className='paper' variant='outlined'>
+              <List className='list'>
+                {list.length ? (
+                  list.map((element, index) => (
+                    <Currency data={element} key={`currency-${index}`} />
+                  ))
+                ) : (
+                  <div className='no-results'>no found results</div>
+                )}
+              </List>
+            </Paper>
+          ) : isError ? (
+            <>something went wrong</>
+          ) : null}
+        </Box>
+      ) : (
+        <div className='no-results'>loading...</div>
+      )}
+    </>
   );
 };
 
